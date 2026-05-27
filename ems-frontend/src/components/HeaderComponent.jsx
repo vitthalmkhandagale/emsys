dffb13016b8412ca3b15b1714d2b054df630cf0a
Sameer Dhumal <sameerdhumal.sd@gmail.com>
2026-05-27 13:00:30 +0530

    Heade component added

--- ems-frontend/src/components/HeaderComponent.jsx
+++ ems-frontend/src/components/HeaderComponent.jsx
@@ -1,1 +1,15 @@
-rafce
+import React from 'react'
+
+const HeaderComponent = () => {
+  return (
+    <div>
+        <header>
+            <nav class="navbar navbar-dark bg-primary">
+                    <a class="navbar-brand" style={{ marginLeft: '20px' }} >Employee Management System</a>
+            </nav>
+        </header>
+    </div>
+  )
+}
+
+export default HeaderComponent
