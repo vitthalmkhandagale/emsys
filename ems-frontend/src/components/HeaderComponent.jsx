14f78a63c36b2998043af3370a093bb984c51e08
Sameer Dhumal <sameerdhumal1495@gmail.com>
2026-05-30 08:16:49 +0530

    Navlink added

--- ems-frontend/src/components/HeaderComponent.jsx
+++ ems-frontend/src/components/HeaderComponent.jsx
@@ -1,11 +1,23 @@
 import React from 'react'
+import { NavLink } from 'react-router-dom'
 
 const HeaderComponent = () => {
   return (
     <div>
         <header>
-            <nav class="navbar navbar-dark bg-primary">
-                    <a class="navbar-brand" style={{ marginLeft: '20px' }} >Employee Management System</a>
+            <nav className="navbar navbar-dark navbar-expand-lg bg-primary">
+                    <a className="navbar-brand" style={{ marginLeft: '20px' }} >Employee Management System</a>
+                    <div className="collapse navbar-collapse" id="navbarNav">
+                      <ul className="navbar-nav">
+                        <li className="nav-item">
+                          <NavLink className="nav-link" to="/employees">Employees</NavLink>
+                        </li>
+                        <li className="nav-item">
+                          <NavLink className="nav-link" to="/departments">Department</NavLink>
+                        </li>
+                        
+                      </ul>
+                    </div>
             </nav>
         </header>
     </div>
